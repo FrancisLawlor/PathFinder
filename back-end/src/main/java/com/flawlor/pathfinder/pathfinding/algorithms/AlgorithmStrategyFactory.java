23665f18c2caf02ac2adc229bfec219cf5c781c7
Francis Lawlor <franlawlor0@gmail.com>
2019-05-18 18:32:53 +0100

    Refactor factory to use enum

--- back-end/src/main/java/com/flawlor/pathfinder/pathfinding/algorithms/AlgorithmStrategyFactory.java
+++ back-end/src/main/java/com/flawlor/pathfinder/pathfinding/algorithms/AlgorithmStrategyFactory.java
@@ -4,16 +4,13 @@ import com.flawlor.pathfinder.pathfinding.algorithms.strategies.BreadthFirstSear
 import com.flawlor.pathfinder.pathfinding.algorithms.strategies.DepthFirstSearch;
 
 public class AlgorithmStrategyFactory {
-    private static final String DEPTH_FIRST_SEARCH = "depth_first_search";
-    private static final String BREADTH_FIRST_SEARCH = "breadth_first_search";
-
     public AlgorithmStrategy createAlgorithmStrategy(String algorithmStrategy) {
         AlgorithmStrategy strategy = null;
 
         if (algorithmStrategy != null) {
-            if (DEPTH_FIRST_SEARCH.equalsIgnoreCase(algorithmStrategy)) {
+            if (Algorithm.DEPTH_FIRST_SEARCH.getCode().equalsIgnoreCase(algorithmStrategy)) {
                 strategy = new DepthFirstSearch();
-            } else if (BREADTH_FIRST_SEARCH.equalsIgnoreCase(algorithmStrategy)) {
+            } else if (Algorithm.BREADTH_FIRST_SEARCH.getCode().equalsIgnoreCase(algorithmStrategy)) {
                 strategy = new BreadthFirstSearch();
             } else {
                 System.err.println("Unknown algorithm strategy");
